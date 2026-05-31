7877eff976e3a35ed96f324f7034dcac0d874033
Sahithi Mayukha Najana <sahithimayukha@gmail.com>
2026-05-31 12:58:20 +0530

    Updated for  BorahaeHQ ARIRANG public release

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Link from "next/link";
 import { PublicFooter } from "@/components/app/public-footer";
+import { Analytics } from "@vercel/analytics/next";
 
 const featureCards = [
   {
